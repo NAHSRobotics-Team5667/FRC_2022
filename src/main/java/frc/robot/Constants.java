ed2e6c0e8b52a6487349fef985844feadc727f02
Pranav Chitiveli <39918567+PVCPipe@users.noreply.github.com>
2022-03-07 17:53:21 -0500

    Drivetrain placeholder constants

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -12,4 +12,13 @@ package frc.robot;
  * <p>It is advised to statically import this class (or one of its inner classes) wherever the
  * constants are needed, to reduce verbosity.
  */
-public final class Constants {}
+public final class Constants {
+    public final class DriveConstants {
+        public final static int kFrontLeftId = -1;
+        public final static int kFrontRightId = -1;
+        public final static int kBackLeftId = -1;
+        public final static int kFrontRightId = -1;
+        
+        public final static double kGearRatio = 8 / 62;
+    }
+}
