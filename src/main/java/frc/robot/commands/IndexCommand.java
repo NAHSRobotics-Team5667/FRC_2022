726fc8f30ec628261bbcbbfc2452299885898cba
Team5667-Drive laptop 2 <46071866+Team5667@users.noreply.github.com>
2022-03-09 16:36:42 -0500

    index has been birthed

--- src/main/java/frc/robot/commands/IndexCommand.java
+++ src/main/java/frc/robot/commands/IndexCommand.java
@@ -3,26 +3,49 @@
 // the WPILib BSD license file in the root directory of this project.
 
 package frc.robot.commands;
-
+import frc.robot.utils.Controller;
+import frc.robot.Constants;
 import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.RobotContainer;
+import frc.robot.subsystems.IndexSubsysytem;
 
 public class IndexCommand extends CommandBase {
+  private IndexSubsysytem index;
+
   /** Creates a new IndexCommand. */
-  public IndexCommand() {
+  public IndexCommand(IndexSubsysytem index) {
+    
     // Use addRequirements() here to declare subsystem dependencies.
+    this.index = index;
+    addRequirements(index);
   }
 
   // Called when the command is initially scheduled.
   @Override
-  public void initialize() {}
+  public void initialize() {
+    if (RobotContainer.controller.getRightTrigger() > 0) {
+      index.indexSpeed(.31);
+      index.indexGoPew(false);
+    } else if (RobotContainer.controller.getLeftTrigger() > 0)
+      index.indexSpeed(.31);
+      index.indexGoPew(true);
+    } else{
+      index.indexSpeed(0);
+      index.indexGoPew(true);
+    }
 
+  }
   // Called every time the scheduler runs while the command is scheduled.
   @Override
-  public void execute() {}
+  public void execute() {
+
+  }
 
   // Called once the command ends or is interrupted.
   @Override
-  public void end(boolean interrupted) {}
+  public void end(boolean interrupted) {
+    index.indexSpeed(0);
+  }
 
   // Returns true when the command should end.
   @Override
--- src/main/java/frc/robot/subsystems/IndexSubsysytem.java
+++ src/main/java/frc/robot/subsystems/IndexSubsysytem.java
@@ -4,14 +4,41 @@
 
 package frc.robot.subsystems;
 
-import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.NeutralMode;
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonFX;
 
-public class IndexSubsysytem extends SubsystemBase {
+import edu.wpi.first.wpilibj.PneumaticsModuleType;
+import edu.wpi.first.wpilibj.Solenoid;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants;
+
+public class IndexSubsysytem extends SubsystemBase {  
+  public Solenoid s_indexPisston;
+  public WPI_TalonFX m_indexMotor;
+
   /** Creates a new IndexSubsysytem. */
-  public IndexSubsysytem() {}
+  public IndexSubsysytem() {
+    
+   
+    s_indexPisston = new Solenoid(PneumaticsModuleType.REVPH ,Constants.IndexConstants.kSolenoidOfIntake);
+    m_indexMotor = new WPI_TalonFX(Constants.IndexConstants.kSpinnyMotorId);
+    m_indexMotor.setNeutralMode(NeutralMode.Brake);
+    
+  } 
+    
+  public void indexSpeed(double fastness){
+    m_indexMotor.set(ControlMode.PercentOutput, fastness);
+  }
+   
+  public void indexGoPew(boolean pisston) {
+    s_indexPisston.set(pisston);
+  }
 
   @Override
   public void periodic() {
     // This method will be called once per scheduler run
+
+    
   }
 }
